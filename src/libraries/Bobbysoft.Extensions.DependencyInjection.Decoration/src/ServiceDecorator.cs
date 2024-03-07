44f4a300c2e18d51958f5905e779b2d9a032fdb3
RobinAxelsson <axelsson.robin@outlook.com>
2024-03-07 16:52:12 +0100

    chore: remove exclamation null

--- src/libraries/Bobbysoft.Extensions.DependencyInjection.Decoration/src/ServiceDecorator.cs
+++ src/libraries/Bobbysoft.Extensions.DependencyInjection.Decoration/src/ServiceDecorator.cs
@@ -4,166 +4,166 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Bobbysoft.Extensions.DependencyInjection
 {
-public static class ServiceDecorator
-{
-    public static void Decorate<T>(this IServiceCollection services, Func<T, IServiceProvider, T> implementationFactory)
+    public static class ServiceDecorator
     {
-        var subjectDescriptor = RetrieveSubjectDescriptor<T>(services);
-        var decoratedDescriptor = CreateDecoratedServiceDescriptor(services, implementationFactory, subjectDescriptor);
-        UpdateServiceCollection(services, decoratedDescriptor, subjectDescriptor);
-    }
-
-    public static void Decorate<T>(this IServiceCollection services, Func<T, T> implementationFactory)
-    {
-        var subjectDescriptor = RetrieveSubjectDescriptor<T>(services);
-        var decoratedDescriptor =
-            CreateDecoratedServiceDescriptor(services, FactoryAdapter(implementationFactory), subjectDescriptor);
-        UpdateServiceCollection(services, decoratedDescriptor, subjectDescriptor);
-    }
-
-    private static ServiceDescriptor CreateDecoratedServiceDescriptor<T>(
-        IServiceCollection services,
-        Func<T, IServiceProvider, T> implementationFactory,
-        ServiceDescriptor subjectDescriptor)
-    {
-        ServiceDescriptor newDescriptor = null!;
-
-        if (ObjectDescriptor(subjectDescriptor))
+        public static void Decorate<T>(this IServiceCollection services, Func<T, IServiceProvider, T> implementationFactory)
         {
-            newDescriptor = ObjectDescriptorCreate(implementationFactory,
-                (T) subjectDescriptor.ImplementationInstance!, subjectDescriptor.Lifetime);
+            var subjectDescriptor = RetrieveSubjectDescriptor<T>(services);
+            var decoratedDescriptor = CreateDecoratedServiceDescriptor(services, implementationFactory, subjectDescriptor);
+            UpdateServiceCollection(services, decoratedDescriptor, subjectDescriptor);
         }
 
-        else if (FactoryDescriptor(subjectDescriptor))
+        public static void Decorate<T>(this IServiceCollection services, Func<T, T> implementationFactory)
         {
-            newDescriptor = FactoryDecoratorDescriptorCreate(
-                implementationFactory, subjectDescriptor.ImplementationFactory!, subjectDescriptor.Lifetime);
+            var subjectDescriptor = RetrieveSubjectDescriptor<T>(services);
+            var decoratedDescriptor =
+                CreateDecoratedServiceDescriptor(services, FactoryAdapter(implementationFactory), subjectDescriptor);
+            UpdateServiceCollection(services, decoratedDescriptor, subjectDescriptor);
         }
 
-        else if (GenericsDescriptor(subjectDescriptor))
+        private static ServiceDescriptor CreateDecoratedServiceDescriptor<T>(
+            IServiceCollection services,
+            Func<T, IServiceProvider, T> implementationFactory,
+            ServiceDescriptor subjectDescriptor)
         {
-            newDescriptor = GenericsDecoratorDescriptorCreate(implementationFactory,
-                subjectDescriptor.ImplementationType!, subjectDescriptor.Lifetime);
-            RepointSubjectDescriptor(services, subjectDescriptor.ImplementationType!, subjectDescriptor.Lifetime);
+            ServiceDescriptor newDescriptor = null;
+
+            if (ObjectDescriptor(subjectDescriptor))
+            {
+                newDescriptor = ObjectDescriptorCreate(implementationFactory,
+                    (T) subjectDescriptor.ImplementationInstance, subjectDescriptor.Lifetime);
+            }
+
+            else if (FactoryDescriptor(subjectDescriptor))
+            {
+                newDescriptor = FactoryDecoratorDescriptorCreate(
+                    implementationFactory, subjectDescriptor.ImplementationFactory, subjectDescriptor.Lifetime);
+            }
+
+            else if (GenericsDescriptor(subjectDescriptor))
+            {
+                newDescriptor = GenericsDecoratorDescriptorCreate(implementationFactory,
+                    subjectDescriptor.ImplementationType, subjectDescriptor.Lifetime);
+                RepointSubjectDescriptor(services, subjectDescriptor.ImplementationType, subjectDescriptor.Lifetime);
+            }
+
+            return newDescriptor;
         }
 
-        return newDescriptor;
-    }
-
-    private static ServiceDescriptor RetrieveSubjectDescriptor<T>(IServiceCollection services)
-    {
-        ServiceDescriptor descriptor;
-
-        try
+        private static ServiceDescriptor RetrieveSubjectDescriptor<T>(IServiceCollection services)
         {
-            descriptor = services.Single(x => x.ServiceType == typeof(T));
-        }
-        catch (Exception e)
-        {
-            var count = services.Count(x => x.ServiceType == typeof(T));
-            throw new ServiceDecorationException("To be able to decorate exactly one descriptor is allowed with" +
-                                                 $" the target service. Target: {typeof(T).Name} count: {count}", e);
-        }
+            ServiceDescriptor descriptor;
 
-        if (ImplementationSameAsServiceType(descriptor))
-            throw new ServiceDecorationException(
-                "You must not decorate a descriptor with same service type as " +
-                $"implementation, you can only decorate an abstracted type");
+            try
+            {
+                descriptor = services.Single(x => x.ServiceType == typeof(T));
+            }
+            catch (Exception e)
+            {
+                var count = services.Count(x => x.ServiceType == typeof(T));
+                throw new ServiceDecorationException("To be able to decorate exactly one descriptor is allowed with" +
+                                                     $" the target service. Target: {typeof(T).Name} count: {count}", e);
+            }
 
-        return descriptor!;
-    }
+            if (ImplementationSameAsServiceType(descriptor))
+                throw new ServiceDecorationException(
+                    "You must not decorate a descriptor with same service type as " +
+                    $"implementation, you can only decorate an abstracted type");
 
-    private static Func<T, IServiceProvider, T> FactoryAdapter<T>(Func<T, T> implementationFactory)
-    {
-        return (subject, provider) => implementationFactory(subject);
-    }
-
-    private static void UpdateServiceCollection(IServiceCollection services, ServiceDescriptor decoratedDescriptor,
-        ServiceDescriptor subjectDescriptor)
-    {
-        services.Add(decoratedDescriptor);
-        services.Remove(subjectDescriptor);
-    }
-
-    private static ServiceDescriptor FactoryDecoratorDescriptorCreate<T>(
-        Func<T, IServiceProvider, T> decoration, Func<IServiceProvider, object> implementationFactory,
-        ServiceLifetime lifetime)
-    {
-        object Factory(IServiceProvider provider)
-        {
-            var subject = (T) implementationFactory(provider);
-            return decoration(subject, provider)!;
+            return descriptor;
         }
 
-        return new ServiceDescriptor(typeof(T), Factory, lifetime);
-    }
-
-    private static ServiceDescriptor GenericsDecoratorDescriptorCreate<T>(
-        Func<T, IServiceProvider, T> decoration, Type implementationType, ServiceLifetime lifetime)
-    {
-        object Factory(IServiceProvider provider)
+        private static Func<T, IServiceProvider, T> FactoryAdapter<T>(Func<T, T> implementationFactory)
         {
-            var subject = GetRequiredImplementation<T>(provider, implementationType);
-            return decoration(subject, provider)!;
+            return (subject, provider) => implementationFactory(subject);
         }
 
-        return new ServiceDescriptor(typeof(T), Factory, lifetime);
-    }
-
-    private static ServiceDescriptor ObjectDescriptorCreate<T>(
-        Func<T, IServiceProvider, T> decoration,
-        T instance,
-        ServiceLifetime lifetime)
-    {
-        object Factory(IServiceProvider provider)
+        private static void UpdateServiceCollection(IServiceCollection services, ServiceDescriptor decoratedDescriptor,
+            ServiceDescriptor subjectDescriptor)
         {
-            return decoration(instance, provider)!;
+            services.Add(decoratedDescriptor);
+            services.Remove(subjectDescriptor);
         }
 
-        return new ServiceDescriptor(typeof(T), Factory, lifetime);
-    }
+        private static ServiceDescriptor FactoryDecoratorDescriptorCreate<T>(
+            Func<T, IServiceProvider, T> decoration, Func<IServiceProvider, object> implementationFactory,
+            ServiceLifetime lifetime)
+        {
+            object Factory(IServiceProvider provider)
+            {
+                var subject = (T) implementationFactory(provider);
+                return decoration(subject, provider);
+            }
 
-    private static T GetRequiredImplementation<T>(IServiceProvider provider, Type implementationType)
-    {
-        T subject;
-        try
-        {
-            subject = (T) provider.GetRequiredService(implementationType);
-        }
-        catch (Exception e)
-        {
-            throw new ServiceDecorationException(
-                $"Failed to get required service subject to decorate. ServiceType: {typeof(T).Name}", e);
+            return new ServiceDescriptor(typeof(T), Factory, lifetime);
         }
 
-        return subject;
-    }
+        private static ServiceDescriptor GenericsDecoratorDescriptorCreate<T>(
+            Func<T, IServiceProvider, T> decoration, Type implementationType, ServiceLifetime lifetime)
+        {
+            object Factory(IServiceProvider provider)
+            {
+                var subject = GetRequiredImplementation<T>(provider, implementationType);
+                return decoration(subject, provider);
+            }
 
-    private static bool ImplementationSameAsServiceType(ServiceDescriptor descriptor)
-    {
-        return descriptor.ImplementationType != null && descriptor.ServiceType == descriptor.ImplementationType;
-    }
+            return new ServiceDescriptor(typeof(T), Factory, lifetime);
+        }
 
-    private static void RepointSubjectDescriptor(
-        IServiceCollection services, Type descriptorImplementationType, ServiceLifetime descriptorLifetime)
-    {
-        services.Add(new ServiceDescriptor(descriptorImplementationType, descriptorImplementationType,
-            descriptorLifetime));
-    }
+        private static ServiceDescriptor ObjectDescriptorCreate<T>(
+            Func<T, IServiceProvider, T> decoration,
+            T instance,
+            ServiceLifetime lifetime)
+        {
+            object Factory(IServiceProvider provider)
+            {
+                return decoration(instance, provider);
+            }
 
-    private static bool ObjectDescriptor(ServiceDescriptor descriptor)
-    {
-        return descriptor.ImplementationInstance != null;
-    }
+            return new ServiceDescriptor(typeof(T), Factory, lifetime);
+        }
 
-    private static bool FactoryDescriptor(ServiceDescriptor descriptor)
-    {
-        return descriptor.ImplementationFactory != null;
-    }
+        private static T GetRequiredImplementation<T>(IServiceProvider provider, Type implementationType)
+        {
+            T subject;
+            try
+            {
+                subject = (T) provider.GetRequiredService(implementationType);
+            }
+            catch (Exception e)
+            {
+                throw new ServiceDecorationException(
+                    $"Failed to get required service subject to decorate. ServiceType: {typeof(T).Name}", e);
+            }
 
-    private static bool GenericsDescriptor(ServiceDescriptor descriptor)
-    {
-        return descriptor.ImplementationType != null;
+            return subject;
+        }
+
+        private static bool ImplementationSameAsServiceType(ServiceDescriptor descriptor)
+        {
+            return descriptor.ImplementationType != null && descriptor.ServiceType == descriptor.ImplementationType;
+        }
+
+        private static void RepointSubjectDescriptor(
+            IServiceCollection services, Type descriptorImplementationType, ServiceLifetime descriptorLifetime)
+        {
+            services.Add(new ServiceDescriptor(descriptorImplementationType, descriptorImplementationType,
+                descriptorLifetime));
+        }
+
+        private static bool ObjectDescriptor(ServiceDescriptor descriptor)
+        {
+            return descriptor.ImplementationInstance != null;
+        }
+
+        private static bool FactoryDescriptor(ServiceDescriptor descriptor)
+        {
+            return descriptor.ImplementationFactory != null;
+        }
+
+        private static bool GenericsDescriptor(ServiceDescriptor descriptor)
+        {
+            return descriptor.ImplementationType != null;
+        }
     }
-}
 }
